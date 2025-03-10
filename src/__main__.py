3855c80f227d0fafdd64b008d536b617c459afd0
lafamila <lafamila@naver.com>
2025-03-10 23:04:07 +0900

    add: front-end

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+@app.route('/daily')
+def _index():
     return render_template('index.html')
+
+@app.route('/week')
+def _week():
+    return render_template('week.html')
+
+@app.route('/month')
+def _month():
+    return render_template('month.html')
+
+@app.route('/year')
+def _year():
+    return render_template('year.html')
+
 
 if __name__=="__main__":
     app.run(port=20023, debug=True)
